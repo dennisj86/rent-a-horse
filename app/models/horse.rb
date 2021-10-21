342cd289673de7db370356fd1bb147236caefbb6
Martin Krause <findmemart@gmail.com>
2021-10-21 20:01:00 +0200

    added model horse and bookings + validation

--- app/models/horse.rb
+++ app/models/horse.rb
@@ -2,4 +2,5 @@ class Horse < ApplicationRecord
   has_many :bookings
   belongs_to :user
   validates :horse_name, :speed, :height, :obedience presence: true
+  validates :speed, :height numericality: true
 end
