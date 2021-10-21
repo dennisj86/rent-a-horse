7541339d16a459860be253f2a9666110d81984fe
Natalie Bechtold <natalie.bechtold@gmail.com>
2021-10-21 14:30:06 +0200

    Worked on booking model

---
+++ app/models/booking.rb
@@ -0,0 +1,4 @@
+class Booking < ApplicationRecord
+  validates :user_id, :horse_id, presence: :true
+  belongs_to :horse, :user
+end
--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,13 @@ ActiveRecord::Schema.define(version: 2021_10_19_185935) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "bookings", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "horse_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
