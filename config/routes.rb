c97b909da4ecfae00df923f2f5c3632077fbc7de
Dennis <dennisj86@protonmail.com>
2021-10-30 13:44:45 +0200

    bookings and everything that was needed to do

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  resources :bookings, only: %i[index destroy show]
+
   root to: 'pages#home'
   resources :horses, only: %i[index show new create] do
-    resources :bookings, only: %i[show new create destroy]
+    resources :bookings, only: %i[new create]
   end
 end
